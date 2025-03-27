717a7387d8560049185bc51f4d4ce207186696bc
elin-sunhye <tjsgp43@gmail.com>
2025-03-27 16:25:25 +0900

    기본 셋팅

---
+++ src/main/java/com/elin/stocksim_back/StockSimBackApplication.java
@@ -0,0 +1,13 @@
+package com.elin.stocksim_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSimBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elin/stocksim_back/StockSimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elin.stocksim_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
